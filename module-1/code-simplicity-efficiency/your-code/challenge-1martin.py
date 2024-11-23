d8b9983313922c3463e4d9257aa8f7fbbdfebc98
martingonzalezr001 <martinglezrodriguez@gmail.com>
2024-11-23 11:22:03 +0100

    Label advanced

--- module-1/code-simplicity-efficiency/your-code/challenge-1martin.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-1martin.py
@@ -7,163 +7,115 @@ expects.
 
 The code is very long and messy. Refactor it according to what you have learned about
 code simplicity and efficiency.
+
+
+
 """
 
-print('Welcome to this calculator!')
-print('It can add and subtract whole numbers from zero to five')
-a = input('Please choose your first number (zero to five): ')
-b = input('What do you want to do? plus or minus: ')
-c = input('Please choose your second number (zero to five): ')
 
-if a == 'zero' and b == 'plus'  and c == 'zero':
-    print("zero plus zero equals zero")
-if a == 'zero' and b == 'plus'  and c == 'one':
-    print("zero plus one equals one")
-if a == 'zero' and b == 'plus'  and c == 'two':
-    print("zero plus two equals two")
-if a == 'zero' and b == 'plus'  and c == 'three':
-    print("zero plus three equals three")
-if a == 'zero' and b == 'plus'  and c == 'four':
-    print("zero plus four equals four")
-if a == 'zero' and b == 'plus'  and c == 'five':
-    print("zero plus five equals five")
-if a == 'one' and b == 'plus'  and c == 'zero':
-    print("one plus zero equals one")
-if a == 'one' and b == 'plus'  and c == 'one':
-    print("one plus one equals two")
-if a == 'one' and b == 'plus'  and c == 'two':
-    print("one plus two equals three")
-if a == 'one' and b == 'plus'  and c == 'three':
-    print("one plus three equals four")
-if a == 'one' and b == 'plus'  and c == 'four':
-    print("one plus four equals five")
-if a == 'one' and b == 'plus'  and c == 'five':
-    print("one plus five equals six")
-if a == 'two' and b == 'plus'  and c == 'zero':
-    print("two plus zero equals two")
-if a == 'two' and b == 'plus'  and c == 'one':
-    print("two plus one equals three")
-if a == 'two' and b == 'plus'  and c == 'two':
-    print("two plus two equals four")
-if a == 'two' and b == 'plus'  and c == 'three':
-    print("two plus three equals five")
-if a == 'two' and b == 'plus'  and c == 'four':
-    print("two plus four equals six")
-if a == 'two' and b == 'plus'  and c == 'five':
-    print("two plus five equals seven")
-if a == 'three' and b == 'plus'  and c == 'zero':
-    print("three plus zero equals three")
-if a == 'three' and b == 'plus'  and c == 'one':
-    print("three plus one equals four")
-if a == 'three' and b == 'plus'  and c == 'two':
-    print("three plus two equals five")
-if a == 'three' and b == 'plus'  and c == 'three':
-    print("three plus three equals six")
-if a == 'three' and b == 'plus'  and c == 'four':
-    print("three plus four equals seven")
-if a == 'three' and b == 'plus'  and c == 'five':
-    print("three plus five equals eight")
-if a == 'four' and b == 'plus'  and c == 'zero':
-    print("four plus zero equals four")
-if a == 'four' and b == 'plus'  and c == 'one':
-    print("four plus one equals five")
-if a == 'four' and b == 'plus'  and c == 'two':
-    print("four plus two equals six")
-if a == 'four' and b == 'plus'  and c == 'three':
-    print("four plus three equals seven")
-if a == 'four' and b == 'plus'  and c == 'four':
-    print("four plus four equals eight")
-if a == 'four' and b == 'plus'  and c == 'five':
-    print("four plus five equals nine")
-if a == 'five' and b == 'plus'  and c == 'zero':
-    print("five plus zero equals five")
-if a == 'five' and b == 'plus'  and c == 'one':
-    print("five plus one equals six")
-if a == 'five' and b == 'plus'  and c == 'two':
-    print("five plus two equals seven")
-if a == 'five' and b == 'plus'  and c == 'three':
-    print("five plus three equals eight")
-if a == 'five' and b == 'plus'  and c == 'four':
-    print("five plus four equals nine")
-if a == 'five' and b == 'plus'  and c == 'five':
-    print("five plus five equals ten")
+# Arithmetic operations
+def add(n1,n2):
+    sum = n1 + n2
+    return sum
+def subtract(n1,n2):
+    minus = n1 - n2
+    return minus
+
+def parserLetter(n1_str, n2_str ,dict, op):
+    
+
+    '''Set the strings operators into ints to calculate the result and transform it into a string'''
+    n1 = 0
+    n2 = 0
+    
+    for key, value in dict.items():
+        if key == n1_str:
+            n1 = value
+        if key == n2_str:
+            n2 = value
+    print(" N1: " + str(n1))
+    print(" N2: " + str(n2))
+    
+    
+    result_str = ''
+
+    
+    print(type(op))
+    print("Op: ")
+    print(op)
+    if op == 'add' or op == '+' or op == 'sumar' or op == 'suma' or op == 1 or  op == "1" or op == "+" or op == "plus" :
+        result = n1 + n2
+        print(result)
+
+    if op == 'subtract' or op == 'restar' or op == 'resta' or op == '-' or op == 'minus':
+        result = n1 - n2
+        
+
+    else:
+        print("....")
+        return "I cannot make that operation, goodbye :)"
+
+    for key, value in dict.items():
+        print("check check")
+        if value == result:
+            result_str = key
+            print(result_str)
+
+    # Choose arithmetic operation 
+    if op == 'add' or op == '+' or op == 'sumar' or op == 'suma' or op == '1' or op == '+' or op == "plus":
+        print(n1_str + " + " + n2_str  + " = " + result_str)
+
+        return f"{n1_str} + {n2_str} = {result_str}"
+    
+    elif op == 'subtract' or op == 'restar' or op == 'resta' or op == '-' or op == 'minus':
+        print(n1_str + " - " + n2_str  + " = " + result_str)
+        return f"{n1_str} + {n2_str} = {result_str}"
+    else:
+        return "I cannot make that operation, goodbye :)"
 
 
-if a == 'zero' and b == 'minus' and c == 'zero':
-    print("zero minus zero equals zero")
-if a == 'zero' and b == 'minus' and c == 'one':
-    print("zero minus one equals negative one")
-if a == 'zero' and b == 'minus' and c == 'two':
-    print("zero minus two equals negative two")
-if a == 'zero' and b == 'minus' and c == 'three':
-    print("zero minus three equals negative three")
-if a == 'zero' and b == 'minus' and c == 'four':
-    print("zero minus four equals negative four")
-if a == 'zero' and b == 'minus' and c == 'five':
-    print("zero minus five equals negative five")
-if a == 'one' and b == 'minus' and c == 'zero':
-    print("one minus zero equals one")
-if a == 'one' and b == 'minus' and c == 'one':
-    print("one minus one equals zero")
-if a == 'one' and b == 'minus' and c == 'two':
-    print("one minus two equals negative one")
-if a == 'one' and b == 'minus' and c == 'three':
-    print("one minus three equals negative three")
-if a == 'one' and b == 'minus' and c == 'four':
-    print("one minus four equals negative three")
-if a == 'one' and b == 'minus' and c == 'five':
-    print("one minus five equals negative four")
-if a == 'two' and b == 'minus' and c == 'zero':
-    print("two minus zero equals two")
-if a == 'two' and b == 'minus' and c == 'one':
-    print("two minus one equals one")
-if a == 'two' and b == 'minus' and c == 'two':
-    print("two minus two equals zero")
-if a == 'two' and b == 'minus' and c == 'three':
-    print("two minus three equals negative one")
-if a == 'two' and b == 'minus' and c == 'four':
-    print("two minus four equals negative two")
-if a == 'two' and b == 'minus' and c == 'five':
-    print("two minus five equals negative three")
-if a == 'three' and b == 'minus' and c == 'zero':
-    print("three minus zero equals three")
-if a == 'three' and b == 'minus' and c == 'one':
-    print("three minus one equals two")
-if a == 'three' and b == 'minus' and c == 'two':
-    print("three minus two equals one")
-if a == 'three' and b == 'minus' and c == 'three':
-    print("three minus three equals zero")
-if a == 'three' and b == 'minus' and c == 'four':
-    print("three minus four equals negative one")
-if a == 'three' and b == 'minus' and c == 'five':
-    print("three minus five equals negative two")
-if a == 'four' and b == 'minus' and c == 'zero':
-    print("four minus zero equals four")
-if a == 'four' and b == 'minus' and c == 'one':
-    print("four minus one equals three")
-if a == 'four' and b == 'minus' and c == 'two':
-    print("four minus two equals two")
-if a == 'four' and b == 'minus' and c == 'three':
-    print("four minus three equals one")
-if a == 'four' and b == 'minus' and c == 'four':
-    print("four minus four equals zero")
-if a == 'four' and b == 'minus' and c == 'five':
-    print("four minus five equals negative one")
-if a == 'five' and b == 'minus' and c == 'zero':
-    print("five minus zero equals five")
-if a == 'five' and b == 'minus' and c == 'one':
-    print("five minus one equals four")
-if a == 'five' and b == 'minus' and c == 'two':
-    print("five minus two equals three")
-if a == 'five' and b == 'minus' and c == 'three':
-    print("five minus three equals two")
-if a == 'five' and b == 'minus' and c == 'four':
-    print("five minus four equals one")
-if a == 'five' and b == 'minus' and c == 'five':
-    print("five minus five equals zero")
+def select_menu():
+    '''
+    Main menu to select the format operation and if its about numbers calculate them
+    '''
+    n_format = input("What type of format do would you rather:\n 1. Numbers. \n 2. Letters\n")
+    print(n_format)
+    n_format = n_format.lower()
+    
+    if n_format == '1' or n_format == 'numbers'  or n_format == 'nums' or n_format == 'num' or n_format == '':
+        num1 = int(input("Introduce the first number from 1 to 5:\n" ))
+        num2 = int(input("Introduce the second number from 1 to 5:\n" ))
+        type_op = input("What operation would you like to do?\n 1. Add(+)\n 2. Substract(-)\n")
+        
+        if type_op == 'add' or type_op == '+' or type_op == 'sumar' or type_op == 'suma' or type_op == '1' or type_op == '+' or type_op == "plus" :
+            print("The answer is: " + str(num1) + " + " + str(num2) + " = " + str(add(num1,num2)))
+        elif type_op == 'subtract' or type_op == 'restar' or type_op == 'resta' or type_op == '-' or type_op == 'minus':
+            print("The answer is: " + str(num1) + " - " + str(num2) + " = " + str(subtract(num1,num2)))
 
 
-if (not a == 'zero' and not a == 'one' and not a == 'two' and not a == 'three' and not a == 'four' and not a == 'five') or (not c == 'zero' and not c == 'one' and not c == 'two' and not c == 'three' and not c == 'four' and not c == 'five') or (not b == 'plus' and not b == 'minus'):
-    print("I am not able to answer this question. Check your input.")
+    elif n_format == '2' or n_format == 'leters' or n_format == 'letters':
+        letter_num ={ 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'ten':10} 
+        letter_num1 = (input("Introduce the first number from one to five(with letters):\n"))
+        letter_num2 = (input("Introduce the first number from one to five(with letters):\n"))
+        type_op = input("What operation would you like to do?\n 1. Add(+)\n 2. Substract(-)\n")
+        parserLetter(letter_num1,letter_num2,letter_num,type_op)
 
-print("Thanks for using this calculator, goodbye :)")
+    else:
+        print("I am not able to answer this question. Check your input.")
+       
+select_menu()
+
+
+        
+
+
+
+
+
+
+
+    
+    
+
+
+    
